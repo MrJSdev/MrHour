8947a276a8561553f35fc7e01d2368bccf1674be
khanriyaz24 <khanriyaz24@gmail.com>
2020-11-07 11:12:03 +0400

    go api work

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"MrHour/http"
+	"fmt"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	fmt.Println("Hello, Brother")
+	router := gin.Default()
+	router.Use(cors.Default())
+	router.GET("/hours", http.GetHours)
+	router.GET("/messages", http.GetMessages)
+	router.POST("/power", http.GetPower)
+	router.POST("/hour/save", http.SaveHour)
+	router.POST("/message/send", http.SaveMessage)
+	router.Run()
+}
